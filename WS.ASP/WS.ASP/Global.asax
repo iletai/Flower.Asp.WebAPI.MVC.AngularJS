a6a899dabc3b9e29add6b18d422f82d5e072a950
iletai <iletai@hotmail.com>
2018-10-27 11:28:21 +0700

    + First commit : Import prj form local create

---
+++ WS.ASP/WS.ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WS.ASP.MvcApplication" Language="C#" %>
---
+++ WS.ASP/WS.ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WS.ASP.Startup))]
+namespace WS.ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
