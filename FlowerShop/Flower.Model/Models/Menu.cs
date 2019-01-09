316a078dad5111af5f8b61c20ef86e3bfb9f58ec
Tài <iletai@hotmail.com>
2019-01-10 00:28:54 +0700

    ++ Add class Footer, Menu, MenuGroup in project Model

---
+++ FlowerShop/Flower.Model/Models/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Flower.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        public int ID
+        {
+            set; get;
+        }
+
+        [Required]
+        public string Name
+        {
+            set; get;
+        }
+
+        [Required]
+        public string URL
+        {
+            set; get;
+        }
+
+        public int? DisplayOrder //? alow null
+        {
+            set; get;
+        }
+
+        [Required]
+        public int GroupID
+        {
+            set; get;
+        }
+
+        [ForeignKey("GroupID")]
+        public virtual MenuGroup MenuGroups //help point to Menu
+        {
+            set; get;
+        }
+        public string Target
+        {
+            set; get;
+        }
+
+        public bool Status
+        {
+            set; get;
+        }
+    }
+}
---
+++ FlowerShop/Flower.Model/Models/MenuGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Flower.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int ID
+        {
+            set; get;
+        }
+
+        [Required]
+        public string Name
+        {
+            set; get;
+        }
+
+        public virtual IEnumerable<Menu> Menus //create key foriegn
+        {
+            get;set;
+        }
+    }
+}
